b3dedbacad27445019ab62b4ce0dfcd44b108ec0
Danny Hermes <dhermes@blend.com>
2019-08-25 15:08:35 -0700

    Wrap `serve_proxy` in a `KeyboardInterrupt` handler.
    This handler makes sure all connections are "cleaned up".

--- src/tcp_h2_describe/_serve.py
+++ src/tcp_h2_describe/_serve.py
@@ -13,6 +13,7 @@
 import select
 import socket
 import threading
+import time
 
 import tcp_h2_describe._connect
 import tcp_h2_describe._display
@@ -60,19 +61,22 @@ def accept(non_blocking_socket):
     return client_socket, client_addr
 
 
-def serve_proxy(proxy_port, server_port, server_host=DEFAULT_SERVER_HOST):
+def _serve_proxy(proxy_port, server_port, server_host, update_threads):
     """Serve the proxy.
 
-    This should run as a top-level server and CLI invocations of
-    ``tcp-h2-describe`` will directly invoke it.
+    This is a "happy path" implementation for ``serve_proxy`` that doesn't
+    worry about interrupt handling (e.g. ``KeyboardInterrupt``).
 
     Args:
         proxy_port (int): A legal port number that the caller has permissions
             to bind to.
         server_port (int): A port number for a running "server" process.
-        server_host (Optional[str]): The host name where the server process is
+        server_host (str): The host name where the server process is
             running (i.e. the server that is being proxied). Defaults to
             ``localhost``.
+        update_threads (Callable[[threading.Thread], None]): A callable that
+            takes a single thread and does not return. Used to track state
+            of the request handling threads by external caller.
     """
     proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     proxy_socket.setblocking(0)
@@ -95,3 +99,78 @@ def serve_proxy(proxy_port, server_port, server_host=DEFAULT_SERVER_HOST):
             args=(client_socket, client_addr, server_host, server_port),
         )
         t_handle.start()
+        update_threads(t_handle)
+
+
+class UpdateThreads:
+    """Closure around list of currently active threads.
+
+    An instance of this class is expected to be used by ``_serve_proxy()``
+    as the ``update_threads`` argument.
+
+    Args:
+        prune_interval (Optional[float]): Time (in seconds) between "pruning"
+            active request threads. The pruning only occurs when ``__call__``
+            is invoked, which is typically immediately after ACCEPT-ing a new
+            request.
+    """
+
+    def __init__(self, prune_interval=15.0):
+        self.prune_interval = prune_interval
+        self.active_threads = []
+        self.last_prune = time.monotonic()
+
+    def __call__(self, t_handle):
+        """Call this instance with a single thread.
+
+        Args:
+            t_handle (threading.Thread): A newly created thread that is
+                handling a request.
+        """
+        self.active_threads.append(t_handle)
+        # Check if we should prune the active threads.
+        now = time.monotonic()
+        if now - self.last_prune > self.prune_interval:
+            self.prune_inactive()
+            self.last_prune = now
+
+    def prune_inactive(self):
+        """Prune number of active request handling threads.
+
+        This updates ``self.active_threads`` in place.
+        """
+        self.active_threads = [
+            t_handle for t_handle in self.active_threads if t_handle.is_alive()
+        ]
+
+    def wait_all(self):
+        """Wait until all active threads have completed."""
+        for t_handle in self.active_threads:
+            t_handle.join()
+
+
+def serve_proxy(proxy_port, server_port, server_host=DEFAULT_SERVER_HOST):
+    """Serve the proxy.
+
+    This should run as a top-level server and CLI invocations of
+    ``tcp-h2-describe`` will directly invoke it.
+
+    Args:
+        proxy_port (int): A legal port number that the caller has permissions
+            to bind to.
+        server_port (int): A port number for a running "server" process.
+        server_host (Optional[str]): The host name where the server process is
+            running (i.e. the server that is being proxied). Defaults to
+            ``localhost``.
+    """
+    update_threads = UpdateThreads()
+    try:
+        _serve_proxy(proxy_port, server_port, server_host, update_threads)
+    except KeyboardInterrupt:
+        tcp_h2_describe._display.display(
+            f"Stopping tcp-h2-describe proxy server on port {proxy_port}"
+        )
+        tcp_h2_describe._display.display(
+            "Waiting for request handlers to complete..."
+        )
+        update_threads.wait_all()
